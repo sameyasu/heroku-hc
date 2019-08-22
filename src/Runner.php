b77bb693941eeed4531305dda38ec75bdf3968f8
sameyasu <sameyasu@gmail.com>
2019-08-22 22:38:23 +0900

    Add HerokuHC/Runner class (unimplemented)

---
+++ src/Runner.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * HerokuHC/Runner
+ *
+ * @author sameyasu
+ * @version 0.1
+ * @copyright 2019 sameyasu
+ * @package HerokuHC
+ * @license MIT
+ **/
+namespace HerokuHC;
+
+/**
+ * Runner Class
+ **/
+final class Runner
+{
+    public static function start() : void
+    {
+        echo 'Ran';
+    }
+}

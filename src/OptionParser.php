264e1a375f4058e5285e48a4df0ff1952fa62ea3
sameyasu <sameyasu@gmail.com>
2019-09-09 20:43:22 +0900

    Add OptionParser

---
+++ src/OptionParser.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Option Parser Util class
+ *
+ * @author sameyasu
+ * @version 0.1
+ * @copyright 2019 sameyasu
+ * @package HerokuHC
+ * @license MIT
+ **/
+namespace HerokuHC;
+
+/**
+ * Option Parser
+ **/
+final class OptionParser
+{
+    use MonologLoggerTrait;
+
+    const DEFAULT_INTERVAL_IN_SECONDS = 600;
+    const DEFAULT_HOURS = '0-23';
+
+    /**
+     * Constructor
+     **/
+    public function __construct()
+    {
+        $this->initLogger();
+    }
+
+    /**
+     * Parse URL
+     * @param string $url
+     * @return string
+     */
+    public function parseUrl(string $url) : ?string
+    {
+        if (preg_match('|\Ahttps?://|', $url, $matches) === 1) {
+            $this->logger->info('URL', ['url' => $url]);
+            return $url;
+        } else {
+            $this->logger->warning('Invalid URL', ['url' => $url]);
+            return null;
+        }
+    }
+
+    /**
+     * Parse interval option
+     * @param string $interval
+     * @return array tuple (minInterval, maxInterval)
+     */
+    public function parseInterval(string $interval) : array
+    {
+        if (preg_match('/\A(?<min>[0-9]+)-(?<max>[0-9]+)\z/', $interval, $matches) === 1) {
+            return [
+                intval($matches['min']),
+                intval($matches['max']),
+            ];
+        } elseif (is_numeric($interval)) {
+            return [
+                intval($interval),  // min
+                intval($interval),  // max
+            ];
+        } else {
+            $this->logger->warning('Invalid Interval', ['interval' => $interval]);
+            return [
+                self::DEFAULT_INTERVAL_IN_SECONDS,  // min
+                self::DEFAULT_INTERVAL_IN_SECONDS,  // max
+            ];
+        }
+    }
+
+    /**
+     * Parse running hours
+     * @param string $hours
+     * @return array Map of hours
+     */
+    public function parseHours(string $hours) : array
+    {
+        if ($hours === '') {
+            $hours = self::DEFAULT_HOURS;
+        }
+
+        $rangeHours = array_map(
+            function ($h) {
+                if (preg_match('/\A(?<start>[0-9]+)-(?<end>[0-9]+)\z/', $h, $matches) === 1) {
+                    if ($matches['start'] <= $matches['end'] && $matches['start'] >= 0 && $matches['end'] <= 23) {
+                        return range($matches['start'], $matches['end']);
+                    } else {
+                        $this->logger->warning(
+                            'Invalid range',
+                            ['range' => $h, 'start' => $matches['start'], 'end' => $matches['end']]
+                        );
+                        return [];
+                    }
+                } elseif (is_numeric($h)) {
+                    return [intval($h)];
+                } else {
+                    $this->logger->warning('Invalid hour', ['hour' => $h]);
+                    return [];
+                }
+            },
+            explode(',', $hours)
+        );
+
+        $runningHours = array_reduce(
+            $rangeHours,
+            function ($carry, $item) {
+                if (is_array($item)) {
+                    $carry = array_merge($carry, array_values($item));
+                } else {
+                    $carry[] = $item;
+                }
+                return $carry;
+            },
+            []
+        );
+
+        return array_values(
+            array_intersect(
+                range(0, 23),
+                $runningHours
+            )
+        );
+    }
+}
--- src/Runner.php
+++ src/Runner.php
@@ -17,8 +17,6 @@ final class Runner
 {
     use MonologLoggerTrait;
 
-    const DEFAULT_INTERVAL_IN_SECONDS = 600;
-
     /**
      * Constructor
      **/
@@ -49,22 +47,18 @@ final class Runner
      */
     public function run() : void
     {
-        $url = getenv('HC_URL') ?: '';
-        if (preg_match('|\Ahttps?://|', $url, $matches) !== 1) {
-            $this->logger->warning('Invalid URL', ['url' => $url]);
-            return;
+        $parser = new OptionParser();
+        $url = $parser->parseUrl(getenv('HC_URL') ?: '');
+        if ($url === null) {
+            throw new \RuntimeException('URL is invalid');
         }
 
-        $interval = getenv('INTERVAL') ?: '';
-        list($minInterval, $maxInterval) = $this->calculateInterval($interval);
-
-        $hours = getenv('HOURS') ?: '0-23';
-        $runningHours = $this->calculateRunningHours($hours);
+        list($minInterval, $maxInterval) = $parser->parseInterval(getenv('INTERVAL') ?: '');
+        $runningHours = $parser->parseHours(getenv('HOURS') ?: '');
 
         $this->logger->info(
-            'Started',
+            'Started Options',
             [
-                'URL' => $url,
                 'interval' => [$minInterval, $maxInterval],
                 'hours' => $runningHours,
             ]
@@ -80,81 +74,5 @@ final class Runner
         ->setRandomInterval($minInterval, $maxInterval)
         ->setRunningHours($runningHours)
         ->start();
-    }
-
-    /**
-     * Calculate interval option
-     * @param string $interval
-     * @return array tuple
-     */
-    private function calculateInterval(string $interval) : array
-    {
-        if (preg_match('/\A(?<min>[0-9]+)-(?<max>[0-9]+)\z/', $interval, $matches) === 1) {
-            return [
-                intval($matches['min']),
-                intval($matches['max']),
-            ];
-        } elseif (is_numeric($interval)) {
-            return [
-                intval($interval),  // min
-                intval($interval),  // max
-            ];
-        } else {
-            $this->logger->warning('Invalid Interval', ['interval' => $interval]);
-            return [
-                self::DEFAULT_INTERVAL_IN_SECONDS,  // min
-                self::DEFAULT_INTERVAL_IN_SECONDS,  // max
-            ];
-        }
-    }
-
-    /**
-     * Calculate running hours
-     * @param string $hours
-     * @return array Map of hours
-     */
-    private function calculateRunningHours(string $hours) : array
-    {
-        $rangeHours = array_map(
-            function ($h) {
-                if (preg_match('/\A(?<start>[0-9]+)-(?<end>[0-9]+)\z/', $h, $matches) === 1) {
-                    if ($matches['start'] <= $matches['end'] && $matches['start'] >= 0 && $matches['end'] <= 23) {
-                        return range($matches['start'], $matches['end']);
-                    } else {
-                        $this->logger->warning(
-                            'Invalid range',
-                            ['range' => $h, 'start' => $matches['start'], 'end' => $matches['end']]
-                        );
-                        return [];
-                    }
-                } elseif (is_numeric($h)) {
-                    return [intval($h)];
-                } else {
-                    $this->logger->warning('Invalid hour', ['hour' => $h]);
-                    return [];
-                }
-            },
-            explode(',', $hours)
-        );
-
-        $runningHours = array_reduce(
-            $rangeHours,
-            function ($carry, $item) {
-                if (is_array($item)) {
-                    $carry = array_merge($carry, array_values($item));
-                } else {
-                    $carry[] = $item;
-                }
-                return $carry;
-            },
-            []
-        );
-
-        return array_values(
-            array_intersect(
-                range(0, 23),
-                $runningHours
-            )
-        );
     }
 }
